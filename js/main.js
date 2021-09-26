ca80e1b3acbca3b169caf4f085f1de6da4bc8c59
thanhdatio <83289656+thanhdatio@users.noreply.github.com>
2021-09-26 20:55:42 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ const abi = [
 let headsOrTails;
 let ethUsd;
 const deployedNetwork = 97;//To which network is the contract deployed? Ganache: 5777, Ropsten: 3, Mainnet: 1
-const contractAddress = "0xd21727e558Fa7D3e5CE4DaC0b818999f4a52bc03";//Contract address on Ropsten
+const contractAddress = "0x579B54207718144c06c1Ee864CEA574964f92177";//Contract address on Ropsten
 // const contractAddress = "0xEf8A3a8cD1c26C1a36A9C3594A8613c0aF18d499";//Contract address on Ganache
 let provider;
 let signer;
