c1dc9fc394e54ad08d7850c0780663867a068cab
thanhdatio <83289656+thanhdatio@users.noreply.github.com>
2021-09-27 22:51:09 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -536,7 +536,7 @@ async function play(headsOrTailsSelection, amountToBetEther) {
   //Reload contract variable in case user has changed account in Metamask after page load.
   headsOrTails = new ethers.Contract(contractAddress, abi, provider.getSigner());
 	TokenContract = new ethers.Contract(tokenAddress, TokenAbi, provider.getSigner());
-	TokenContract.approve(contractAddress,amountToBetEther);
+	TokenContract.approve(contractAddress,amountToBetEther*100000000);
   //Define some custom settings when initiating the contract function
   let overrides = {
     // The maximum units of gas for the transaction to use
