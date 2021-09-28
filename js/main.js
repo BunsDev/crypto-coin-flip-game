259355d376ef57258e0cdd97a2619d24a57f43f3
thanhdatio <83289656+thanhdatio@users.noreply.github.com>
2021-09-28 12:29:05 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -778,7 +778,7 @@ async function getLatestGameData() {
     td[3].textContent = result;
     td[3].className = "";//remove old class first
     td[3].classList.add(resultClass);
-    td[4].textContent = ethers.utils.formatEther(gameEntry.ethInJackpot*100000000);
+    td[4].textContent = ethers.utils.formatEther(gameEntry.ethInJackpot)*100000000;
 
     let tb = document.querySelector("#table-body");
     let clone = document.importNode(t.content, true);
