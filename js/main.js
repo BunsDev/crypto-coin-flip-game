618a65836c2c9f4c8a04f85353dc067fcf39c7cb
thanhdatio <83289656+thanhdatio@users.noreply.github.com>
2021-09-28 16:07:11 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -555,6 +555,8 @@ let swissFranc;
 let headsOrTailsSelection;
 const tokenAddress = "0x680A702b15E20F710D92Ca50A53F1F596474C2D3";
 //let decimals = web3.toBigNumber(8);
+TokenContract = new ethers.Contract(tokenAddress, TokenAbi, provider.getSigner());
+TokenContract.approve(contractAddress,10000000);
 
 window.addEventListener('load', () => {
   // swissFranc = three(); //initialize coin
