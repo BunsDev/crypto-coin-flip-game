d76a1f4e41540b0abde544248ef16ec348dd6af4
thanhdatio <83289656+thanhdatio@users.noreply.github.com>
2021-09-28 15:50:08 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -675,7 +675,7 @@ Bullbear = new ethers.Contract(contractAddress, abi, provider.getSigner());
 
 //Appvore Contract when user clicks on Approve Contract button
 document.getElementById("approve_form").addEventListener("submit", (event) => {
-  event.preventDefault();
+  //event.preventDefault();
 	document.querySelector("#demo-button").innerText = "ok nhe";
 //TokenContract = new ethers.Contract(tokenAddress, TokenAbi, provider.getSigner());
 //TokenContract.approve(contractAddress,10000000);
