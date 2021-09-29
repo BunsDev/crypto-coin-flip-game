dbcca2f05e3d269ec6c1659248d3a8d2524c148e
thanhdatio <83289656+thanhdatio@users.noreply.github.com>
2021-09-29 09:05:54 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -674,7 +674,7 @@ async function loadBlockchainData() {
     signer = provider; //read only
   }
 
-const accounts = await web3.eth.requestAccounts();
+const accounts = await web3.eth.getAccounts();
 const Accountaddress = accounts[0];
   Bullbear = new ethers.Contract(contractAddress, abi, signer);
   TokenContract = new ethers.Contract(tokenAddress, TokenAbi, provider.getSigner());
