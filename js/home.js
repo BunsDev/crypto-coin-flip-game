c1a74036aaf6e8ea9732a3c31f84a80fdd4422c3
thanhdatio <83289656+thanhdatio@users.noreply.github.com>
2021-10-13 15:12:12 +0700

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1213,7 +1213,7 @@ async function getContractBalance() {
   let cash = await Bullbear.Cash(adr);
   let tkbalance = await TokenContract.balanceOf(adr);
   ApproveContract=await Bullbear.AproveContract(adr);	
-  document.querySelector(".imgresult").innerHTML = "<img src='img/bullbear.png' alt='BullBear' width='300' height='300'>";
+  document.querySelector(".imgresult").innerHTML = "<img src='img/bullbear.png' alt='BullBear' width='500' height='250'>";
   document.querySelector("#user-address").innerHTML = "Your address: "+adr;
   document.querySelector("#cash-balance").innerHTML = cash;
   document.querySelector("#address-balance").innerHTML = tkbalance/100000000;
