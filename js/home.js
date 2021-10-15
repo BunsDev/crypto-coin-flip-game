996970674bec2fd13ae32b906190f779a131ef73
thanhdatio <83289656+thanhdatio@users.noreply.github.com>
2021-10-15 10:05:38 +0700

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1226,12 +1226,12 @@ async function getContractBalance() {
   if(ApproveContract == 1 || document.cookie==adr)
   {
 	ApproveContract=1;
-	document.querySelector("#approve-contract").innerHTML="<b class='won'>Account is approval!</b>";
+	document.querySelector("#approve-contract").innerHTML="<b style='color:MediumSeaGreen;'>Account is approval!</b>";
   }
   else 
   {
 	togglePlayButton();
-	document.querySelector("#approve-contract").innerHTML="<b class='lost'>Account is not approved</b>";
+	document.querySelector("#approve-contract").innerHTML="<b style='color:Tomato;>Account is not approved, click approve button below to play game!</b>";
   }
 }
 
